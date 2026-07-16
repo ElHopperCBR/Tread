4e32fff05e8df9ffb6b02efe6be76169747b5e79
caiocas <caio.alves.jj@gmail.com>
2026-07-16 18:40:03 -0300

    Tarefa1

--- tests.cs
+++ tests.cs
@@ -2,11 +2,13 @@ using static System.Console;
 
 WriteLine("Iniciando o programa de testes...");
 
-Thread minhaThread = new Thread(PreparaCafe);
+Thread minhaThread = new Thread(PreparaCafe); //Para gerenciar operação assincrona, é necessário criar uma thread separada para a operação de preparo do café.
 
 minhaThread.Start();
 
 WriteLine("Enquanto o café está sendo preparado, você pode fazer outras coisas...");
+Thread.Sleep(3000);
+WriteLine("Pode atender outros clientes, ler um livro, ou até mesmo tomar um banho.");
 ReadKey();
 
 
