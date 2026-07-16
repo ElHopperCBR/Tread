1dec96b8622fdd8b5302374c448281aa25bd2a4c
ElHopperCBR <cesar.brandao@sp.senai.br>
2026-07-16 18:56:08 -0300

    método main sem await

--- tests.cs
+++ tests.cs
@@ -1,20 +1,35 @@
 using static System.Console;
 
-WriteLine("Iniciando o programa de testes...");
-
-Thread minhaThread = new Thread(PreparaCafe); //Para gerenciar operação assincrona, é necessário criar uma thread separada para a operação de preparo do café.
-
-minhaThread.Start();
-
-WriteLine("Enquanto o café está sendo preparado, você pode fazer outras coisas...");
-Thread.Sleep(3000);
-WriteLine("Pode atender outros clientes, ler um livro, ou até mesmo tomar um banho.");
-ReadKey();
-
-
-static void PreparaCafe()
+static async Task Main()
 {
-    WriteLine("Preparando o café...");
-    Thread.Sleep(2000);
+    WriteLine("Pedido realizado.");
+    Task cafe = PrepararCafe();
+    Task hamburguer = PrepararHamburguer();
+    Task batata = PrepararBatata();
+    await Task.WhenAll(cafe, hamburguer, batata);
+    WriteLine();
+    WriteLine("Pedido entregue.");
+}
+
+static async Task PrepararCafe()
+{
+    WriteLine("Preparando café...");
+    await Task.Delay(2000);
     WriteLine("Café pronto!");
 }
+
+static async Task PrepararHamburguer()
+{
+    WriteLine("Preparando hambúrguer...");
+    await Task.Delay(3000);
+    WriteLine("Hambúrguer pronto!");
+}
+
+static async Task PrepararBatata()
+{
+    WriteLine("Preparando batata...");
+    await Task.Delay(2500);
+    WriteLine("Batata pronta!");
+}
+
+Main(); // Chamada do método Main para iniciar a execução do programa, porém sem aguardar sua conclusão, pois Main é um método assíncrono.
